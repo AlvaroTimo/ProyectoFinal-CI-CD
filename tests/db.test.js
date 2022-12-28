893fde9734a9b2c8bd186e48d6c574bdebc07a29
AlvaroTimo <alvarojoelticonamotta@gmail.com>
2022-12-27 19:32:38 -0500

    Agregando nuevos test

---
+++ tests/db.test.js
@@ -0,0 +1,5 @@
+const main = require("../src/public/js/main.js")
+
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+});
--- tests/index.test.js
+++
@@ -1,9 +0,0 @@
-const request = require ("supertest")
-const app = require("../src/app.js")
-
-describe('GET /',()=>{
-    test("should respond with a 200 status code",async ()=>{
-        const response = await request(app).get('/').send()
-        expect(response.statusCode).toBe(200)
-    })
-})
